6ef9817f71b8ec709c3582a8240405ed0f68a7ac
fransiskaab <fransiskaagustinabunjamin_2428240154@mhs.mdp.ac.id>
2026-04-06 13:10:11 +0700

    localstorage

---
+++ localstrorage/scriptls.js
@@ -0,0 +1,48 @@
+let npm = document.getElementById("npm");
+let nama = document.getElementById("nama");
+
+function simpan(){
+    console.log(npm.value)
+    console.log(nama.value)
+
+    // localStorage.setItem("npm", npm.value)
+    // localStorage.setItem("nama",nama.value)
+
+    //jika local storage belum ada isi/value
+    if(localStorage.getItem("mahasiswa")===null){
+        // simpan array kosong [] 
+        localStorage.setItem("mahasiswa", "[]")
+    }
+
+    // panggil local storage (konvesi string -> object)
+    let data = JSON.parse(localStorage.getItem("mahasiswa"))
+    console.log(data)
+
+    // simpan value npm dan nama ke dalam object data
+    data.push({
+        npm: npm.value,
+        nama: nama.value
+    })
+    console.log(data)
+
+    // simpan data terbaru ke dalam local storage
+    // koversi dari object ke string
+    localStorage.setItem("mahasiswa", JSON.stringify(data))
+}
+
+function tampil(){
+    // panggil dulu local storage
+    let hasil = JSON.parse(localStorage.getItem("mahasiswa"))
+
+    // clear element ul id=list-mhs
+    document.getElementById("list-mhs").innerHTML = ""
+
+    // lakukan perulangan (forEach)
+    hasil.forEach(element => {
+        // console.log(element)
+        document.getElementById("list-mhs").innerHTML = `<li>${element.npm} ${element.nama}</li>`
+    });
+}
+
+//  jalankan fuction tampil()
+    tampil()
